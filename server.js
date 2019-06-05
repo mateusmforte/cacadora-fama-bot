7adee26dcf95d8fd7a8bbbafd23a6ad918579b71
Mateus Moura Forte <m-forte@hotmail.com>
2019-06-05 18:00:58 -0300

    Novas funcionalidades (#1)
    * Testando lib discord.js
    
    * Adicionando funcionalidades novas
    
    Trap, Novo usuário entrando no servidor

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.get('/', function(req, res) {
-	res.send('<h1>hello world</h1>');
-});
-
-app.listen(3000);
-console.log('Servidor rodando na porta 3000');
